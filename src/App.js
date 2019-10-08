7df712e5584d4c09673012d4f424b7860642e1c4
Omulosi <mulongojohnpaul@gmail.com>
2019-10-08 10:47:12 +0300

    Use context API to consume data in ShoppingCart and Navigation

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import data from './data';
 
 // contexts
 import { ProductContext } from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext';
 
 // Components
 import Navigation from './components/Navigation';
@@ -21,17 +22,16 @@ function App() {
 
 	return (
 		<ProductContext.Provider value={{products, addItem}}>
-		  <div className="App">
-		  	<Navigation cart={cart} />
-		  
-		  	{/* Routes */}
-        <Route exact path="/" component={Products} />
-
-		  	<Route
-		  		path="/cart"
-		  		render={() => <ShoppingCart cart={cart} />}
-		  	/>
-		  </div>
+		  <CartContext.Provider value={cart}>
+		    <div className="App">
+		    	<Navigation />
+		    
+		    	{/* Routes */}
+          <Route exact path="/" component={Products} />
+          <Route exact path="/cart" component={ShoppingCart} />
+		    
+		    </div>
+		  </CartContext.Provider>
 		</ProductContext.Provider>
 	);
 }
